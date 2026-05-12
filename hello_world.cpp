16d67179caa78326fc1e3ca296550883a397cee5
destr0f <testdas@mail.ru>
2026-05-12 23:09:54 +0300

    updated hello_world.cpp to ask for name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-cout << "Hello world!";
+cout << "Hello world!" << endl;
+
+string name;
+cout << "Please enter name: ";
+cin >> name;
+
+cout << "Hello world from " << name << endl;
 }
